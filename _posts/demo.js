cc2e6a3a67224da8e7d3b5e98589441b89f47ff6
kanxue2002 <kanxue2002@gmail.com>
2016-08-01 13:32:48 +0800

    test2

--- _posts/demo.js
+++ _posts/demo.js
@@ -2,19 +2,19 @@ require('UILabel,UIColor,UIFont');
 defineClass('YXLoginViewController', {
             viewDidLoad: function() {
            self.super().viewDidLoad();
-           // Do any additional setup after loading the view.
-           // var label = UILabel.new();
-           // label.setTextColor(UIColor.redColor());
-           // label.setBackgroundColor(UIColor.greenColor());
-           // label.setText("I am JSPatch");
-           // label.setFont(UIFont.systemFontOfSize(30));
-           // label.sizeToFit();
-           // label.setCenter(self.view().center());
-           // self.view().addSubview(label);
-            var alertView = require('UIAlertView').alloc`().init();
-            alertView.setTitle('Alert');
-            alertView.setMessage('AlertView from js');
-            alertView.addButtonWithTitle('OK');
-            alertView.show();
+            // Do any additional setup after loading the view.
+            var label = UILabel.new();
+            label.setTextColor(UIColor.redColor());
+            label.setBackgroundColor(UIColor.greenColor());
+            label.setText("I am JSPatch");
+            label.setFont(UIFont.systemFontOfSize(30));
+            label.sizeToFit();
+            label.setCenter(self.view().center());
+            self.view().addSubview(label);
+            // var alertView = require('UIAlertView').alloc`().init();
+            // alertView.setTitle('Alert');
+            // alertView.setMessage('AlertView from js');
+            // alertView.addButtonWithTitle('OK');
+            // alertView.show();
             },
             });
